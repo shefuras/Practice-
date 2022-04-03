09a98a597e673dffdbb6a11a1d657305a7a7fc67
shefuras <shefuras@gmail.com>
2022-04-02 21:16:53 -0400

    first commit

---
+++ src/basic/KeyWord.java
@@ -0,0 +1,18 @@
+package basic;
+
+public class KeyWord {
+    /*>Data Type - Integer, String, Boolean, Char, Short, Long
+    > int/String book = 10/"midsummer night" - here book is variable and 10/midnight is value
+    >public String car (String Hyundai) - here String Hyundai is the parameter
+    >static int pen ; - static variable
+    >String pencil ; - instance variable
+    >public void nails (){
+    int polish ; - local variable
+    }
+     */
+}
+/* For new project git init
+                   git commit -m "any message"
+                   git add .
+                   git remote url
+ */
--- src/basic/Method.java
+++ src/basic/Method.java
@@ -7,8 +7,7 @@ public class Method {
        >You can never write any method inside the psvm block because psvm is also a method
         so you can never write a method inside a method.
        > Anytime when we want to call the property/method of the class, we have to use the object.
-       > When we declare a method inside a class we need a body but if we declare a method outside
-         of a class we don't need a body.
+       > When we declare a method inside a class we need a body.
         */
      public static void main(String[] args) {
          Method method = new Method();
@@ -20,7 +19,35 @@ public class Method {
          pencil();
          /*we don't need to create any object & constructor because static method belong to class
                          or it is closely related*/
+
+         System.out.println(method.bag(5, 6));
+         int finalBag = method.bag(1, 2);
+         System.out.println(finalBag);
+         String company = method.phone("samsung");
+         System.out.println(company);
+         int powerGlass = method.glass(10, 5);
+         System.out.println(powerGlass);
+         String electronic = method.laptop("HP has both laptops and computers");
+         System.out.println(electronic);
+         int wig = method.hair(10000, 50);
+         System.out.println(wig);
+         String beaches = method.florida("Tampa has many beautiful beaches");
+         System.out.println(beaches);
+         System.out.println(color(3,4));
+         int sky = color(3,4);
+         System.out.println(sky);
+         String metal = bottle("we should recycle plastic bottles");
+         System.out.println(metal);;
+         method.ball = "cricket";
+         System.out.println(method.ball);
+         System.out.println(method.bat);
+         System.out.println(mouse);
+         moon = "in the sky";
+         System.out.println(moon);
+         pet();
+         method.shoe("size 8");
      }
+
     //Non Static Void Type//
         public void book() {
         System.out.println("It's a book");
@@ -32,6 +59,59 @@ public class Method {
     public static void pencil (){
         System.out.println("Pencil is incomplete without eraser");
     }
+
+    /*>Difference between Return and Void type - data in return type can be carried anywhere but
+     data in void type cannot be carried anywhere, it stays where it is.
+     >Anything written inside parenthesis is known as Parameter.
+     >We always have to store data in return type.
+
+     */
+ //Non Static Return Type//
+    public int bag (int a, int b) {
+        int totalbag = a+b;
+        return totalbag;
+    }
+    public String phone (String s) {
+        String company = s;
+        return company;
+    }
+    public int glass (int s, int g) {
+        int sunglass = s*g;
+        return sunglass;
+    }
+    public String laptop (String l){
+        String computer =l;
+        return computer;
+    }
+    public int hair (int h, int o) {
+        int oil = h/o;
+        return oil;
+    }
+    public String florida (String f) {
+        String tampa = f;
+        return tampa;
+    }
+    //Static Return Type//
+    public static int color (int y, int p) {
+        int rainbow = y+p;
+        return rainbow;
+    }
+    public static String bottle (String b){
+        String plastic = b;
+        return plastic;
+    }
+    String ball;
+    String bat = "6";
+        static String mouse = "logitech";
+                static String moon;
+
+                public static void pet (){
+                    int f = 8;
+                    System.out.println(f);
+                }
+                public void shoe (String bata){
+                    System.out.println(bata);
+                }
 
 
 }
